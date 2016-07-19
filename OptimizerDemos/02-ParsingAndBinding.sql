802eaabf929ec3b31e1cfcc4f1370f05040b4590
Brian Hansen <brian@tf3604.com>
2016-07-19 11:37:43 -0500

    Added MIT license.

--- OptimizerDemos/02-ParsingAndBinding.sql
+++ OptimizerDemos/02-ParsingAndBinding.sql
@@ -2,9 +2,22 @@
 -- 02-ParsingAndBinding.sql
 -----------------------------------------------------------------------------------------------------------------------
 -- Copyright 2016, Brian Hansen (brian@tf3604.com).
--- Feel free to use this code in any way you see fit, but do so at your own risk.
 -- Version 1.0.1
 -- Look for the most recent version of this script at www.tf3604.com/optimizer.
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+-- documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+-- the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+-- and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+-- 
+-- The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+-- of the Software.
+-- 
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+-- TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+-- THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+-- CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+-- DEALINGS IN THE SOFTWARE.
 -----------------------------------------------------------------------------------------------------------------------
 
 -- Turn on PARSEONLY.  This directs the query processor to only execute the "parse" phase.
