815e248d65ea2e27bc781be9ee79d8f8b85b65f9
Brian Hansen <brian@tf3604.com>
2016-07-28 20:54:52 -0500

    Add "QueryCounts" query into solution.

--- OptimizerDemos/00-Prep.sql
+++ OptimizerDemos/00-Prep.sql
@@ -5,3 +5,5 @@ move 'CorpDB' to 'c:\data\sql2016\data\CorpDB.mdf',
 move 'CorpDB_log' to 'c:\data\sql2016\log\CorpDB_log.ldf';
 
 alter database CorpDB set compatibility_level = 130;
+
+dbcc freeproccache;
