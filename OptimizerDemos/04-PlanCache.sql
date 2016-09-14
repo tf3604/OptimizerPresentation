8ec9b1daeea406e468964a9f69b683a74950d5f2
Brian Hansen <brian@tf3604.com>
2016-09-13 20:11:59 -0500

    Change scripts for KC presentation.

--- OptimizerDemos/04-PlanCache.sql
+++ OptimizerDemos/04-PlanCache.sql
@@ -39,7 +39,7 @@ go
 select *
 from CorpDB.dbo.OrderHeader oh
 inner join CorpDB.dbo.Customer c on c.CustomerID = oh.CustomerId
-where c.State = 'OK'
+where c.State in ('KS', 'MO')
 and c.FirstName = 'Mary';
 go
 
@@ -60,7 +60,7 @@ go
 select *
 from CorpDB.dbo.OrderHeader oh
 inner join CorpDB.dbo.Customer c on c.CustomerID = oh.CustomerId
-where c.State = 'OK'
+where c.State in ('KS', 'MO')
 and c.FirstName = 'Mary';
 go
 set quoted_identifier on;
--- OptimizerDemos/07-OptimizerInfo.sql
+++ OptimizerDemos/07-OptimizerInfo.sql
@@ -91,7 +91,7 @@ select top 10 od.ProductId, sum(od.Quantity) - 20 ExcessOrders
 from dbo.OrderHeader oh
 inner join dbo.OrderDetail od on oh.OrderId = od.OrderId
 inner join dbo.Customer cust on oh.CustomerId = cust.CustomerID
-where cust.State = 'OK'
+where cust.State in ('KS', 'MO')
 group by od.ProductId
 having sum(od.Quantity) >= 20
 order by od.ProductId
@@ -135,7 +135,7 @@ go
 select oh.OrderId, oh.OrderDate, c.FirstName, c.LastName
 from CorpDB.dbo.OrderHeader oh
 inner join CorpDB.dbo.Customer c on oh.CustomerId = c.CustomerID
-where c.State = 'OK'
+where c.State in ('KS', 'MO')
 option (recompile);
 go
 select * into #after_optimizer_info from sys.dm_exec_query_optimizer_info;
@@ -174,7 +174,7 @@ with CustomersOfInterest as
 (
 	select c.CustomerID, c.FirstName, c.LastName
 	from CorpDB.dbo.Customer c
-	where c.State = 'OK'
+	where c.State in ('KS', 'MO')
 )
 select oh.OrderId, oh.OrderDate, c.FirstName, c.LastName
 from CorpDB.dbo.OrderHeader oh
