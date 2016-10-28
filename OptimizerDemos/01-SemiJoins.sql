d03a489f4d289f118c4d2f91e44fc9639823337b
Brian Hansen <brian@tf3604.com>
2016-10-28 08:49:17 -0500

    Bump revision number on scripts.

--- OptimizerDemos/01-SemiJoins.sql
+++ OptimizerDemos/01-SemiJoins.sql
@@ -2,7 +2,7 @@
 -- 01-SemiJoins.sql
 -----------------------------------------------------------------------------------------------------------------------
 -- Copyright 2016, Brian Hansen (brian@tf3604.com).
--- Version 1.0.3
+-- Version 1.0.4
 -- Look for the most recent version of this script at www.tf3604.com/optimizer.
 --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
