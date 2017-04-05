b53edec82e01dfee62666d706a80ae3cafe28c4e
Brian Hansen <brian@tf3604.com>
2017-04-05 09:11:07 -0500

    Add simple demo for TF2363.

---
+++ OptimizerDemos/14-TF2363.sql
@@ -0,0 +1,36 @@
+-----------------------------------------------------------------------------------------------------------------------
+-- 14-TF2363.sql
+-----------------------------------------------------------------------------------------------------------------------
+-- Copyright 2016-2017-2017, Brian Hansen (brian@tf3604.com).
+-- Version 1.0.5
+-- Look for the most recent version of this script at www.tf3604.com/optimizer.
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+-- documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+-- the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+-- and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+-- 
+-- The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+-- of the Software.
+-- 
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+-- TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+-- THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+-- CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+-- DEALINGS IN THE SOFTWARE.
+-----------------------------------------------------------------------------------------------------------------------
+
+dbcc traceon (3604);
+
+use CorpDB;
+
+select top 5 od.ProductId, sum(od.Quantity) - 20 ExcessOrders
+from dbo.OrderHeader oh
+inner join dbo.OrderDetail od on oh.OrderId = od.OrderId
+inner join dbo.Customer cust on oh.CustomerId = cust.CustomerID
+where cust.State = 'WI'
+group by od.ProductId
+having sum(od.Quantity) >= 20
+order by od.ProductId
+option (recompile, querytraceon 2363);
+
