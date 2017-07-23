b4d939d4543df1ef9187dccf4d8ada7367cb9687
Brian Hansen <brian@tf3604.com>
2017-07-23 17:21:16 -0500

    Change demo scripts for Louisiana.

--- OptimizerDemos/03-TrivialPlans.sql
+++ OptimizerDemos/03-TrivialPlans.sql
@@ -33,7 +33,7 @@ from CorpDB.dbo.Customer c;
 
 select *
 from CorpDB.dbo.Customer c
-where c.State = 'NE';
+where c.State = 'LA';
 
 -- Now let's add an index on State.
 
@@ -48,7 +48,7 @@ create index idx_Customer__State on CorpDB.dbo.Customer (State);
 
 select *
 from CorpDB.dbo.Customer c
-where c.State = 'NE';
+where c.State = 'LA';
 
 -- Cleanup
 
--- OptimizerDemos/04-PlanCache.sql
+++ OptimizerDemos/04-PlanCache.sql
@@ -39,7 +39,7 @@ go
 select *
 from CorpDB.dbo.OrderHeader oh
 inner join CorpDB.dbo.Customer c on c.CustomerID = oh.CustomerId
-where c.State = 'NE'
+where c.State = 'LA'
 and c.FirstName = 'Mary';
 go
 
@@ -60,7 +60,7 @@ go
 select *
 from CorpDB.dbo.OrderHeader oh
 inner join CorpDB.dbo.Customer c on c.CustomerID = oh.CustomerId
-where c.State = 'NE'
+where c.State = 'LA'
 and c.FirstName = 'Mary';
 go
 set quoted_identifier on;
