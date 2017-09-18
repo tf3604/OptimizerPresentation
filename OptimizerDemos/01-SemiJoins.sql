da9b9fd92f102029f259c05351f4cb473673d333
Brian Hansen <brian@tf3604.com>
2017-09-17 19:40:25 -0500

    Bump version number.

--- OptimizerDemos/01-SemiJoins.sql
+++ OptimizerDemos/01-SemiJoins.sql
@@ -2,7 +2,7 @@
 -- 01-SemiJoins.sql
 -----------------------------------------------------------------------------------------------------------------------
 -- Copyright 2016-2017, Brian Hansen (brian@tf3604.com).
--- Version 1.0.7
+-- Version 1.0.10
 -- Look for the most recent version of this script at www.tf3604.com/optimizer.
 --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
