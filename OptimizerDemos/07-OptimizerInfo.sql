8592fbca425e28ece6576f147658795134997f20
Brian Hansen <brian@tf3604.com>
2018-07-23 09:23:23 -0500

    Bump version number.

--- OptimizerDemos/07-OptimizerInfo.sql
+++ OptimizerDemos/07-OptimizerInfo.sql
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------------------------------------------------------
 -- 07-OptimizerInfo.sql
--- Version 1.0.12
+-- Version 1.0.13
 -- Look for the most recent version of this script at www.tf3604.com/optimizer
 -- MIT License.  See the bottom of this file for details.
 -----------------------------------------------------------------------------------------------------------------------
