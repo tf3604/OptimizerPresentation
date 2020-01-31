15ad43d4d0e325c93adf1956bf9f5dd07e53f9df
Brian Hansen <brian@tf3604.com>
2020-01-31 08:33:24 -0600

    Bump version number.

--- OptimizerDemos/04-PlanCache.sql
+++ OptimizerDemos/04-PlanCache.sql
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------------------------------------------------------
 -- 04-PlanCache.sql
--- Version 1.0.16
+-- Version 1.0.17
 -- Look for the most recent version of this script at www.tf3604.com/optimizer
 -- MIT License.  See the bottom of this file for details.
 -----------------------------------------------------------------------------------------------------------------------
