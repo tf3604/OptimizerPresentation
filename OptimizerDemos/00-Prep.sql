4baffc80307610a8b4a5820fc032d644dbfe5ddb
Brian Hansen <brian@tf3604.com>
2020-06-07 15:53:29 -0500

    Update paths and compatibility in restore statement.

--- OptimizerDemos/00-Prep.sql
+++ OptimizerDemos/00-Prep.sql
@@ -1,9 +1,11 @@
-restore filelistonly from disk = 'C:\data\sql2014\backup\CorpDB.bak';
+-- restore filelistonly from disk = 'C:\data\sql2014\backup\CorpDB.bak';
 
-restore database CorpDB from disk = 'C:\data\sql2016\backup\CorpDB.bak' with replace,
-move 'CorpDB' to 'c:\data\sql2016\data\CorpDB.mdf',
-move 'CorpDB_log' to 'c:\data\sql2016\log\CorpDB_log.ldf';
+restore database CorpDB
+from disk = 'CorpDB.bak' --'C:\data\sql2016\backup\CorpDB.bak'
+with replace,
+move 'CorpDB' to 'c:\sql\sql2019\data\CorpDB.mdf',
+move 'CorpDB_log' to 'c:\sql\sql2019\log\CorpDB_log.ldf';
 
-alter database CorpDB set compatibility_level = 130;
+alter database CorpDB set compatibility_level = 150;
 
 dbcc freeproccache;
